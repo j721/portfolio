71d9f4c461394418f72b6a4e256488e85129e3bb
j721 <juliadvu@gmail.com>
2020-10-01 00:16:40 -0500

    updates to styling for website

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import NavBar from '../src/components/NavBar';
-import  Projects  from "../src/components/Projects";
-import  Contact  from "../src/components/Contact";
+import NavBar from "../src/components/NavBar";
+import Projects from "../src/components/Projects";
+import Contact from "../src/components/Contact";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm Julia.</h1>
-      <p>
-        I'm a fullstack developer who loves to learning new technologies and
-        create beautiful projects.
-      </p>
-      <NavBar/>
-      
-      <Projects/>
+      <div className="Contents-Container">
+        <h1>Hi, I'm Julia.</h1>
+        <p>
+          I'm a fullstack developer who loves to learning new technologies and
+          create beautiful projects.
+        </p>
+        <NavBar />
 
-      <Contact/>
+        <Projects />
 
+        <Contact />
+      </div>
     </div>
   );
 }
